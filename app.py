810bff3098e851a536f2239118ca3f1b783257ca
Biangg <88219675+Biangg@users.noreply.github.com>
2025-08-03 17:00:18 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,80 @@
+
+from flask import Flask, session, jsonify, render_template, make_response, g, request
+import datetime as dt
+import pandas as pd
+import sqlite3, models, json
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'APOCALIPTO'
+DATABASE = 'database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.route('/add', methods=['POST'])
+def add():
+    if request.method == 'POST':
+        elemento = request.form['elemento']
+        if elemento == "articulo":
+            nombre = request.form['nombre']
+            describcion = request.form['describcion']
+            precio = request.form['precio']
+            categoria = request.form['categoria']
+            models.Crear.articulo(nombre, describcion, precio, categoria)
+            return jsonify({"estado" : "ok"})
+        nombre = request.form["nombre"]
+        models.Crear.categoria(nombre)
+        return jsonify({"estado" : "ok"})
+
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        usuario = models.Usuarios()
+        try:
+            usuario = request.form["usuario"]
+            password = request.form["password"]
+            yo = models.Usuarios.yo(usuario, password)
+            if yo.empty:
+                return None
+            else:
+                session["usuario"] = usuario
+                session["password"] = password
+                response = {'status': 200, 'usuario': usuario}
+                return jsonify(response)
+        except:
+            return None
+    if 'password' in session and 'usuario' in session:
+        yo = models.Usuarios.yo(session['usuario'], session['password'])
+        categorias = models.Cargador.categorias()
+        return make_response(render_template('ventas.html', yo=yo, categorias = categorias))
+    return render_template('login.html')
+
+@app.route('/ventas')
+def ventas():
+    if 'password' in session and 'usuario' in session:
+        yo = models.Usuarios.yo(session['usuario'], session['password'])
+        categorias = models.Cargador.categorias()
+        return make_response(render_template('ventas.html', categorias = categorias))
+    return render_template('login.html')
+
+@app.route('/articulos')
+def articulos():
+    if 'password' in session and 'usuario' in session:
+        yo = models.Usuarios.yo(session['usuario'], session['password'])
+        categorias = models.Cargador.categorias()
+        return make_response(render_template('articulos.html', categorias = categorias))
+    return render_template('login.html')
+
+@app.route('/cargar', methods=['POST'])
+def cargar():
+    if request.method == 'POST':
+        categoria = request.form["folder"]
+        cargar = models.Cargador.articulo(categoria)
+        tabla = json.loads(cargar.to_json(orient='records'))
+        return jsonify(tabla)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=80)
---
+++ models.py
@@ -0,0 +1,101 @@
+
+import datetime as dt
+import psycopg2
+import pandas as pd
+import pymysql
+import pymysql.cursors
+
+
+# Configura las variables de entorno
+URL = "postgresql://postgres.aaayhwqxqyklufnvpqnj:#Admin_root0@aws-0-us-west-1.pooler.supabase.com:6543/postgres"
+
+db_config = {
+    'host' : 'localhost',
+    'user' : 'celest',
+    'password' : '',
+    'database' : 'eclipse',
+    'cursorclass' : pymysql.cursors.DictCursor
+}
+
+# Función para obtener la conexión a PostgreSQL
+def get_connection():
+    #conn = psycopg2.connect(URL)
+    conn = pymysql.connect(**db_config)
+    return conn 
+
+class Crear:
+    def categoria(nombre):
+        conexion = get_connection()
+        try:
+            with conexion.cursor() as cursor:
+                cursor.execute("""
+                    INSERT INTO categorias (nombre_categoria)
+                    VALUES ( %s)
+                """, (nombre))
+                conexion.commit()
+        finally:
+            conexion.close()
+    
+    def articulo(nombre, describcion, precio, categoria):
+        conexion = get_connection()
+        try:
+            with conexion.cursor() as cursor:
+                cursor.execute("""
+                    INSERT INTO productos (nombre, describcion, precio_unitario, id_categoria)
+                    VALUES (%s, %s, %s, %s)
+                """, (nombre, describcion, int(precio), int(categoria)))
+                conexion.commit()
+        finally:
+            conexion.close()
+    
+
+class Usuarios:
+    def insertar(nombre, password):
+        conexion = get_connection()
+        try:
+            with conexion.cursor() as cursor:
+                cursor.execute("""
+                    INSERT INTO usuarios (nombre, password)
+                    VALUES (%s, %s)
+                """, (nombre, password))
+                conexion.commit()
+        finally:
+            conexion.close()
+
+    def yo(usuario, password):
+        conexion = get_connection()
+        try:
+            with conexion.cursor() as cursor:
+                cursor.execute("SELECT * FROM usuarios WHERE nombre = %s AND password = %s", (usuario, password))
+                columns = [desc[0] for desc in cursor.description]
+                yo = cursor.fetchall()
+        finally:
+            conexion.close()
+        
+        # Retornar un DataFrame vacío si no se encuentra el usuario
+        return pd.DataFrame(yo, columns = columns)
+
+class Cargador:
+    def categorias():
+        conexion = get_connection()
+        try:
+            with conexion.cursor() as cursor:
+                cursor.execute("SELECT * FROM categorias")
+                columns = [desc[0] for desc in cursor.description]
+                categorias = cursor.fetchall()
+        finally:
+            conexion.close()
+        # Retornar un DataFrame vacío si no se encuentra el usuario
+        return pd.DataFrame(categorias, columns = columns)
+    
+    def articulo(categoria):
+        conexion = get_connection()
+        try:
+            with conexion.cursor() as cursor:
+                cursor.execute("SELECT * FROM productos WHERE id_categoria = %s", (int(categoria)))
+                columns = [desc[0] for desc in cursor.description]
+                articulos = cursor.fetchall()
+        finally:
+            conexion.close()
+        # Retornar un DataFrame vacío si no se encuentra el usuario
+        return pd.DataFrame(articulos, columns = columns)
